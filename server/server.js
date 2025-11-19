a9ad812d8b015f2f3f4402d2424fc85ca6db0ccc
mr-mizz <jmuli866@gmail.com>
2025-11-19 16:57:52 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -47,6 +47,5 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
 socketHandler(io);
 
-server.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
-});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
